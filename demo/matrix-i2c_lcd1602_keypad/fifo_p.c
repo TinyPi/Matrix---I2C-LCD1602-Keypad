33ca211858839c28dcbc3cbe573135386693414f
RunningKey1989 <qdstudio@163.com>
2015-12-23 18:02:16 -0800

    add inc/lib

---
+++ demo/matrix-i2c_lcd1602_keypad/fifo_p.c
@@ -0,0 +1,72 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <errno.h>
+ #include <unistd.h>
+ #include <fcntl.h>
+ #include "./fifo_p.h"
+
+#define BUFF_SIZE 1024
+
+//write_p
+int write_p(const char *filename, int mode,char *buf, int len) {
+
+         int fd;
+         int real_write;
+
+//check
+        if(access(filename,F_OK) == 0) {
+          if((mkfifo(filename,mode)<0) && (errno != EEXIST)) {
+            printf("Can NOT create fifo file!\n");
+            return -1;
+         }
+        }
+
+//open
+        if((fd=open(filename,mode)) == -1) {
+        printf("Open fifo error!\n");
+        return -1;
+        }
+//write
+        if ((real_write = write(fd,buf,len)) > 0) {
+         printf("Write into pipe: '%s'.\n",buf);
+        }
+
+     close(fd);
+     return 0;
+}
+
+// read fifo_p
+char *read_p(const char *filename, int mode) {
+
+         int fd;
+         int real_read;
+         char buf[BUFF_SIZE];
+
+//check
+         if(access(filename,F_OK) == -1) {
+          if((mkfifo(filename,mode) < 0)&&(errno != EEXIST)) {
+            printf("Can NOT create fifo file!\n");
+            return -1;
+          }
+        }
+
+//open
+        if((fd=open(filename,mode|O_NONBLOCK)) == -1){
+         printf("Open fifo error!\n");
+         return -1;
+        }
+
+//read
+       do {
+         memset(buf,0,sizeof(buf));
+         if ((real_read = read(fd,buf,sizeof(buf)))>0) {
+             printf("Read from pipe: '%s'.\n",buf);
+         }
+       } while (real_read > 0);
+
+     close(fd);
+     return buf;
+}
