2166506e7fafc5fa6f602ad90d0f2197bd9f567f
wangzhou <wang.zhou@ck-telecom.com>
2016-01-06 19:00:31 +0800

    add auto start

--- demo/matrix-i2c_lcd1602_keypad/Matrix-lcd1602_keypad.c
+++ demo/matrix-i2c_lcd1602_keypad/Matrix-lcd1602_keypad.c
@@ -16,13 +16,30 @@
 #include <sys/reboot.h>
 #include <pthread.h>
 #include "libfahw.h"
+#include <sys/sem.h>//包含信号量定义的头文件  
+
 #define TIME_STR_BUFSIZE     32
+ 
+//联合类型semun定义  
+union semun{  
+    int val;  
+    struct semid_ds *buf;  
+    unsigned short *array;  
+};
+
+//函数声明  
+static int get_semvalue(void); 
+static int set_semvalue(int val);  
+static void del_semvalue(void);   
+static int sem_id;//信号量ID  
 
 
 int server_sockfd = -1;//服务器端套接字
 int client_sockfd = -1;//客户端套接字
 int devFD = 0;
 int revcmd = 0;
+int ipFlg = 0;
+
 char revbuf[BUFSIZ] = {0};
 
 pthread_t thread[2];
@@ -57,6 +74,43 @@ void LCD1602KeyClear(int dev)
 	mydebug("clear screen\n");
 }
 */
+//函数：设置信号量的值  
+static int set_semvalue(int val)  
+{  
+    union semun sem_union;  
+    sem_union.val = val;  
+  
+    if(semctl(sem_id, 0, SETVAL, sem_union))
+	{
+		return 0;
+	}		
+          
+    return 1;  
+}  
+  
+//函数：get信号量的值  
+static int get_semvalue(void)  
+{  
+    union semun sem_union;    
+  	int ret = 0;
+    if(ret = semctl(sem_id,0,GETVAL,sem_union))
+	{  
+		printf("sem val: %d\n", ret);
+        return ret;  
+	}  
+    return -1;  
+} 
+
+//函数：删除信号量  
+static void del_semvalue(void)  
+{  
+    union semun sem_union;  
+  
+    if(semctl(sem_id,0,IPC_RMID,sem_union))
+	{
+		mydebug("Failed to delete semaphore\n");
+	}		 
+} 
 
 unsigned long GetLocalAddr(int dev, char *eth)
 {
@@ -94,6 +148,14 @@ int CreatServer(unsigned long serverip, int *pserver_sockfd, int *pclient_sockfd
 	struct sockaddr_in remote_addr; //客户端网络地址结构体
 	int sin_size;
 	
+	if (client_sockfd > 0 && server_sockfd > 0)
+	{
+		pthread_mutex_lock(&mut);
+		close(client_sockfd);
+		close(server_sockfd);
+		pthread_mutex_unlock(&mut);
+	}
+
 	memset(&my_addr,0,sizeof(my_addr)); //数据初始化--清零
 	my_addr.sin_family=AF_INET; //设置为IP通信
 	my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -123,14 +185,9 @@ int CreatServer(unsigned long serverip, int *pserver_sockfd, int *pclient_sockfd
 	mydebug("wait client...\n");
 	if((client_sockfd = accept(server_sockfd,(struct sockaddr *)&remote_addr,&sin_size))<0)
 	{
-		perror("accept\n");
+		mydebug("accept client error\n");
 		return -1;
 	}
-	
-	LCD1602KeyClear(devFD);
-	usleep(1000);
-	LCD1602KeyDispStr(devFD, 0, 0, "Client success ");
-	LCD1602KeyDispStr(devFD, 0, 1, "  Connectivity ");
 	
 	mydebug("accept client %s\n",inet_ntoa(remote_addr.sin_addr));
 	//len = send(client_sockfd,"Welcome to nanopi\n",21,0);//发送欢迎信息
@@ -144,6 +201,7 @@ int CreatServer(unsigned long serverip, int *pserver_sockfd, int *pclient_sockfd
 	
 	return 0;
 }
+
 /*
         F2(up)        
 F1(left)        F4(right)   F5(ok)
@@ -159,11 +217,15 @@ void *KeyThread1()
     int lastKeyValue = -1;
     int showDefault = 1;
     int needClear = 1;
-	int conMsg = 0;
+	int conMsg = 0x01;
 	int _revcmd = 0;
-	
 	unsigned long serverip = 0;
+	int keyok = 2;
 
+    sem_id = semget((key_t)1234,1,0666 | IPC_CREAT);
+	set_semvalue(66);
+	mydebug("sem_id: %d\n", sem_id);
+	
     if ((devFD = LCD1602KeyInit()) == -1) 
 	{
         mydebug("Fail to init LCD1602\n");
@@ -189,37 +251,31 @@ void *KeyThread1()
 				LCD1602KeyDispStr(devFD, 0, 1, revbuf+24);
 				revbuf[24] = '\0';
                 LCD1602KeyDispStr(devFD, 0, 0, revbuf+8);
-			break;
+				break;
 			
 			case 0x2:
-				_revcmd = 0;
 				keyValue = 0x1d;
-			break;
+				break;
 			
 			case 0x3:
-				_revcmd = 0;
 				keyValue = 0x1b;
-			break;
+				break;
 			
 			case 0x4:
-				_revcmd = 0;
 				keyValue = 0x1e;
-			break;
+				break;
 			
 			case 0x5:
-				_revcmd = 0;
 				keyValue = 0x17;
-			break;
+				break;
 			
 			case 0x6:
-				_revcmd = 0;
 				keyValue = 0x0f;
-			break;
+				break;
 			
 			default:
 				keyValue = LCD1602GetKey(devFD);
-
-			break;
+				break;
 		}
         if ((keyValue != lastKeyValue) && (keyValue != 0x1f)) 
 		{
@@ -244,9 +300,22 @@ void *KeyThread1()
 			showDefault = 0;
 			LCD1602KeyClear(devFD);
 			usleep(1000);
-			LCD1602KeyDispStr(devFD, 0, 0, "IP");
-			LCD1602KeyDispStr(devFD, 0, 1, "Restart");
-            conMsg = 0x0;
+			if (conMsg == 0x1)
+			{
+				LCD1602KeyDispStr(devFD, 0, 0, "Demo      v");
+				LCD1602KeyDispStr(devFD, 0, 1, "Restart    ");
+			}
+			else if (conMsg == 0x2)
+			{
+				LCD1602KeyDispStr(devFD, 0, 0, "Demo       ");
+				LCD1602KeyDispStr(devFD, 0, 1, "Restart   v");
+			}
+			else
+			{
+				LCD1602KeyDispStr(devFD, 0, 0, "Demo      v");
+				LCD1602KeyDispStr(devFD, 0, 1, "Restart    ");
+				conMsg = 0x1;
+			}
             break;
 			
             // F2  up  
@@ -254,8 +323,8 @@ void *KeyThread1()
 		    showDefault = 0;
 			LCD1602KeyClear(devFD);
 			usleep(1000);
-			LCD1602KeyDispStr(devFD, 0, 0, "IP ^");
-			LCD1602KeyDispStr(devFD, 0, 1, "Restart");
+			LCD1602KeyDispStr(devFD, 0, 0, "Demo      v");
+			LCD1602KeyDispStr(devFD, 0, 1, "Restart    ");
             conMsg = 0x1;
             break;
 			
@@ -264,8 +333,8 @@ void *KeyThread1()
             showDefault = 0;
 			LCD1602KeyClear(devFD);
 			usleep(1000);
-			LCD1602KeyDispStr(devFD, 0, 0, "IP");
-			LCD1602KeyDispStr(devFD, 0, 1, "Restart ^");
+			LCD1602KeyDispStr(devFD, 0, 0, "Demo       ");
+			LCD1602KeyDispStr(devFD, 0, 1, "Restart   v");
             conMsg = 0x2;
             break;
 			
@@ -280,7 +349,18 @@ void *KeyThread1()
 					LCD1602KeyDispStr(devFD, 0, 0, "PORT: 8088    ");
 					if ((serverip = GetLocalAddr(devFD, "wlan0")) > 0)
 					{
-						CreatServer(serverip, &server_sockfd, &client_sockfd); // start server
+						if ((keyok == 1) || (keyok == 2))
+						{
+							system("/usr/bin/ckt_analysis&");
+							usleep(10000);
+							system("/usr/bin/mx&");
+                            usleep(10000);
+							keyok = 0;
+						}
+						
+						pthread_mutex_lock(&mut);
+						ipFlg = 1;
+						pthread_mutex_unlock(&mut);
 					}
 					else
 					{
@@ -297,13 +377,19 @@ void *KeyThread1()
             break;
 
             // F5  menu
-        case 0x0f: 
+        case 0x0f:
+		
+			keyok = 1;
+			system ("kill `pidof ckt_analysis`");
+			usleep(10000);
+			system ("kill `pidof mx`");
+			usleep(10000);
 		    showDefault = 0;
 			LCD1602KeyClear(devFD);
 			usleep(1000);
-			LCD1602KeyDispStr(devFD, 0, 0, "IP");
-			LCD1602KeyDispStr(devFD, 0, 1, "Restart");
-            conMsg = 0x0;
+			LCD1602KeyDispStr(devFD, 0, 0, "Demo      v");
+			LCD1602KeyDispStr(devFD, 0, 1, "Restart   ");
+            conMsg = 0x01;
             break;
         }
         if (showDefault == 1) 
@@ -312,7 +398,7 @@ void *KeyThread1()
 			{
                 LCD1602KeyClear(devFD);
                 usleep(1000);
-                LCD1602KeyDispStr(devFD, 0, 0, "IP");
+                LCD1602KeyDispStr(devFD, 0, 0, "Demo      v");
                 LCD1602KeyDispStr(devFD, 0, 1, "Restart    ");
                 needClear = 0;
             }
@@ -324,8 +410,8 @@ void *KeyThread1()
         }
     }    
     mydebug("quit reading key press\n");
-    LCD1602KeyDeInit(devFD);
-
+    LCD1602KeyDeInit(devFD);  
+	del_semvalue();//删除信号量 
 	pthread_exit(NULL);
 }
 
@@ -334,18 +420,24 @@ void *SocketThread2()
 	char buf[BUFSIZ] = { 0 };  //数据传送的缓冲区
 	int len = 0;
 	int _revcmd = 0;
+	unsigned long serverip = 0;
 	
 	mydebug("SocketThread2\n");
 
 	for (; ; )
 	{
 		usleep(100);
-        
+		if (ipFlg)
+		{
+			serverip = GetLocalAddr(devFD, "wlan0");
+			CreatServer(serverip, &server_sockfd, &client_sockfd); // start server
+			pthread_mutex_lock(&mut);
+			ipFlg = 0;
+			pthread_mutex_unlock(&mut);
+		}
 		// 客户端已经连接，准备接收数据
-
 		if (server_sockfd > 0 && client_sockfd > 0)
 		{
-			memset(buf, 0, BUFSIZ);
 			/*接收客户端的数据并将其发送给客户端--recv返回接收到的字节数，send返回发送的字节数*/
 			while ((len = recv(client_sockfd, buf, BUFSIZ, 0)) >0 )
 			{
@@ -388,7 +480,8 @@ void *SocketThread2()
 				
 				pthread_mutex_lock(&mut);
 				revcmd = _revcmd;
-				memcpy(revbuf, buf, BUFSIZ);
+				memset(revbuf, ' ', 40);
+				memcpy(revbuf, buf, len);
 				pthread_mutex_unlock(&mut);
 				
 				usleep(5000);
@@ -405,10 +498,10 @@ void *SocketThread2()
 			close(server_sockfd);
 			
 			pthread_mutex_unlock(&mut);
-			
-			pthread_exit(NULL);
+
 		}
 	}
+	pthread_exit(NULL);
 }
 
 void thread_create(void)
